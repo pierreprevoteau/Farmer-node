72c0f33b93955afe3bef2de0fc6beca38b77f2ac
Pierre Prevoteau <pierreprevoteau@users.noreply.github.com>
2016-03-10 14:22:03 +0100

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,10 @@
 Rails.application.routes.draw do
-  resources :transcodes
   get 'pages/dev'
   get 'pages/overview'
-
   resources :metadata
   resources :settings
   resources :workflows
   resources :media
   resources :transcodes
-
-  mount Resque::Server.new, :at => "/resque"
-
   root to: 'pages#dev'
-
 end
