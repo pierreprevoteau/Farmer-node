6bb4b38d976b4b5eb441c045daa66e7b0d7a303c
Pierre Prevoteau <pierre.prevoteau@askida.fr>
2016-03-11 00:26:55 +0100

    hh

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,2 +1,2 @@
-Resque.redis = Redis.new(:host => ENV['REDIS_HOST'], :port => ENV['REDIS_PORT'], :password => ENV['REDIS_PASSWORD'], :thread_safe => true)
+Resque.redis = Redis.new(:host => ENV['REDIS_HOST'], :port => ENV['REDIS_PORT'], :thread_safe => true)
 Resque.schedule = YAML.load_file("config/schedule.yml")
