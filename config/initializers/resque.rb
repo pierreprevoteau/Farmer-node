c673ca27152aefa2af69a8b6d9a526b8cf98dac8
Pierre Prevoteau <pierre.prevoteau@askida.fr>
2016-03-12 22:26:41 +0100

    Yep

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,4 +1,4 @@
 redis_host = ENV['REDIS_HOST'] || "192.168.99.100"
-redis_port = ENV['REDIS_PORT'] || "32768"
+redis_port = ENV['REDIS_PORT'] || "32777"
 
 Resque.redis = Redis.new(:host => redis_host, :port => redis_port, :thread_safe => true)
