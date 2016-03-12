1548b35c9aed63cc4c0a4732a1446452003fb95e
Pierre Prevoteau <pierre.prevoteau@askida.fr>
2016-03-12 17:57:48 +0100

    work en dockerize

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -2,4 +2,3 @@ redis_host = ENV['REDIS_HOST'] || "192.168.99.100"
 redis_port = ENV['REDIS_PORT'] || "32768"
 
 Resque.redis = Redis.new(:host => redis_host, :port => redis_port, :thread_safe => true)
-Resque.schedule = YAML.load_file("config/schedule.yml")
